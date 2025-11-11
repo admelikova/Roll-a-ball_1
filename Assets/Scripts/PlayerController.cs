0ecf667b4556297a8dea07f1a35298dd64069d1a
Anastasia <amelikova0624@gmail.com>
2025-11-11 17:46:51 -0500

    mod assignment #2 (complete i think)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -31,7 +31,7 @@ public class PlayerController : MonoBehaviour {
 
 
     // variables for dash mechanic
-    public float dashingPower = 50f;
+    public float dashingPower = 12.5f;
     public float dashDuration = 0.5f;
     public float dashCooldown = 3f;
     private bool isDashing = false;
@@ -52,6 +52,13 @@ public class PlayerController : MonoBehaviour {
     public Transform groundCheckPos;
     public Vector3 groundCheckSize = new Vector3(0.5f, 0.05f, 0.5f);
     public LayerMask groundLayer;
+
+
+    // gameobject reference to the pause menu
+    public GameObject pauseMenu;
+
+    // flag variable for pause function
+    public static bool isPaused = false;
 
 
     // variables for audio usage
@@ -76,6 +83,9 @@ public class PlayerController : MonoBehaviour {
         // initially set the win text to be inactive
         winTextObject.SetActive(false);
 
+        //
+        pauseMenu.SetActive(false);
+
         // audio
         audioSource = GetComponent<AudioSource>();
     }
@@ -95,7 +105,7 @@ public class PlayerController : MonoBehaviour {
         }
     */
 
-    // ---------------------------------- FIXED UPDATE FUNCTION -------------------------------------------------------------------------------------------------------------------
+    // ---------------------------------- UPDATE FUNCTION -------------------------------------------------------------------------------------------------------------------
 
     // this function is called once per fixed frame-rate frame
     private void FixedUpdate() {
@@ -115,14 +125,12 @@ public class PlayerController : MonoBehaviour {
 
         // respawn and update lives count
         if (transform.position.y <= -18) {
-            transform.position = new Vector3(0, 0.345f, 0);
-            lives--;
-            SetLivesText();
+            Respawn();
         }
 
         // dash mechanic
         if (isDashing) {
-            rb.AddForce(dashDirection * dashingPower); // * Time.deltaTime?
+            rb.AddForce(dashDirection * dashingPower); // * Time.deltaTime runs weird?
             Debug.Log("dashingggg");
         }
 
@@ -221,12 +229,17 @@ public class PlayerController : MonoBehaviour {
     // function to handle collisions with enemies
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Enemy2") || collision.gameObject.CompareTag("Enemy3") || collision.gameObject.CompareTag("Enemy4")) {
+            // respawn
+            Respawn();
+
+            /*
             // destroy the current object
             Destroy(gameObject);
 
             // update the winText to display "You Lose!"
             winTextObject.gameObject.SetActive(true);
             winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+            */
         }
     }
 
@@ -235,7 +248,7 @@ public class PlayerController : MonoBehaviour {
     // jump function to work with spacebar key input, player jumps up
     public void Jump(InputAction.CallbackContext context) {
         // grounded condition check to prevent infinite jumping
-        if (isGrounded() && !PauseMenu.isPaused) {
+        if (isGrounded() && !isPaused) {
             // if spacebar is pressed
             if (context.performed) {
                 // player jump
@@ -246,7 +259,7 @@ public class PlayerController : MonoBehaviour {
 
     // new move function (previous -> OnMove()), player moves with arrow keys and WASD
     public void Move(InputAction.CallbackContext context) {
-        if (!PauseMenu.isPaused) {
+        if (!isPaused) {
             // read inpute from arrow keys or WASD
             Vector2 movementVector = context.ReadValue<Vector2>();
 
@@ -256,16 +269,32 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
+    //dash function with shift key input, player dashes
     public void Dash(InputAction.CallbackContext context) {
-        if (!PauseMenu.isPaused) {
+        if (!isPaused) {
             if (context.performed && !isDashing && (Time.time >= lastDashTime + dashCooldown)) {
                 StartDash();
             }
         }
     }
 
+    public void Pause(InputAction.CallbackContext context) {
+        if (!isPaused) {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+            isPaused = true;
+        }
+        // (isPaused == true)
+        else {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+            isPaused = false;
+        }
+    }
 
-// ---------------------------------- JUMP HELPER FUNCTIONS -----------------------------------------------------------------------------------------------------------------
+
+
+    // ---------------------------------- JUMP HELPER FUNCTIONS -----------------------------------------------------------------------------------------------------------------
 
     // function to check if player is grounded (touching the ground), to prevent infinite jumping
     private bool isGrounded() {
@@ -283,7 +312,7 @@ public class PlayerController : MonoBehaviour {
     }
 
 
-// ---------------------------------- DASH HELPER FUNCTIONS -----------------------------------------------------------------------------------------------------------------
+    // ---------------------------------- DASH HELPER FUNCTIONS -----------------------------------------------------------------------------------------------------------------
 
     private void StartDash() {
         isDashing = true;
@@ -298,6 +327,32 @@ public class PlayerController : MonoBehaviour {
         isDashing = false;
     }
 
+
+    // ---------------------------------- RESPAWN FUNCTION -----------------------------------------------------------------------------------------------------------------
+
+    public void Respawn() {
+        if (transform.position.x >= -25 && transform.position.z <= 26) {
+            transform.position = new Vector3(0, 0.345f, 0);
+        }
+        else if (transform.position.x >= -25 && transform.position.z <= 65) {
+            transform.position = new Vector3(-14, 2.345f, 30);
+        }
+        else if (transform.position.x >= -35 && transform.position.z >= 65) {
+            transform.position = new Vector3(-12.5f, 5.628f + 0.345f, 85);
+        }
+        else if (transform.position.x <= -35 && transform.position.z >= 65) {
+            transform.position = new Vector3(-57, 10.27f + 0.345f, 86);
+        }
+        else if (transform.position.x <= -25 && transform.position.z <= 65) {
+            transform.position = new Vector3(-60, 10.27f + 0.345f, 53);
+        }
+        else {
+            transform.position = new Vector3(0, 0.345f, 0);
+        }
+
+        lives--;
+        SetLivesText();
+    }
 
 
 }
